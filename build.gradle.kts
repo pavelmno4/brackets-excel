fc9340cf5c4a923d5f6cd8b9229e1f40d23d39f8
Pavel Kozlov <pavel.mno4@gmail.com>
2023-12-26 23:08:14 +0300

    add Compose Multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,21 @@
-val kotlin_version = "1.9.21"
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 val kodein_version = "7.19.0"
 val apache_poi_version = "5.2.5"
 val logback_version = "1.4.11"
 
 plugins {
-    kotlin("jvm") version "1.9.21"
-    application
+    kotlin("jvm") version "1.9.20"
+    id("org.jetbrains.compose") version "1.5.10"
 }
 
 group = "ru.pkozlov"
-version = "0.0.1"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
@@ -21,6 +24,10 @@ dependencies {
 
     /** Excel */
     implementation("org.apache.poi:poi:$apache_poi_version")
+    implementation("org.apache.poi:poi-ooxml:$apache_poi_version")
+
+    /** Desktop */
+    implementation(compose.desktop.currentOs)
 
     /** Logging */
     implementation("org.slf4j:slf4j-api:2.0.9")
@@ -34,8 +41,16 @@ java {
     }
 }
 
-application {
-    mainClass.set("ru.pkozlov.brackets.excel.AppKt")
+compose.desktop {
+    application {
+        mainClass = "AppKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = rootProject.name
+            packageVersion = version as String
+        }
+    }
 }
 
 tasks.named<Test>("test") {
--- src/main/kotlin/ru/pkozlov/brackets.excel/App.kt
+++ src/main/kotlin/ru/pkozlov/brackets.excel/App.kt
@@ -1,5 +1,27 @@
-package ru.pkozlov.brackets.excel
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
-fun main(args: Array<String>) {
+@Preview
+@Composable
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
 
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
 }

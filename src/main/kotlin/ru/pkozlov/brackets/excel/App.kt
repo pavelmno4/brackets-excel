696fd5dd9a09e663fa74c239095f0a1b405a1b17
Pavel Kozlov <pavel.mno4@gmail.com>
2024-01-28 16:05:57 +0300

    test data

--- src/main/kotlin/ru/pkozlov/brackets/excel/App.kt
+++ src/main/kotlin/ru/pkozlov/brackets/excel/App.kt
@@ -26,8 +26,125 @@ fun App() {
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
-    }
+//fun main() = application {
+//    Window(onCloseRequest = ::exitApplication) {
+//        App()
+//    }
+//}
+
+fun main(): Unit {
+    val participants: List<ParticipantDto> = listOf(
+        ParticipantDto(
+            lastName = "Григорьев",
+            firstName = "Игорь",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Кирпич"
+        ),
+        ParticipantDto(
+            lastName = "Алтанин",
+            firstName = "Сергей",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Кирпич"
+        ),
+        ParticipantDto(
+            lastName = "Матросов",
+            firstName = "Константин",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Колючка"
+        ),
+        ParticipantDto(
+            lastName = "Ильин",
+            firstName = "Геннадий",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Антрант"
+        ),
+        ParticipantDto(
+            lastName = "Зайцев",
+            firstName = "Антон",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Лесополоса"
+        ),
+        ParticipantDto(
+            lastName = "Василевский",
+            firstName = "Николай",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Дренаж"
+        ),
+        ParticipantDto(
+            lastName = "Ирпатин",
+            firstName = "Никита",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Дренаж"
+        ),
+        ParticipantDto(
+            lastName = "Валканин",
+            firstName = "Алексей",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Румчик"
+        ),
+        ParticipantDto(
+            lastName = "Афанасьев",
+            firstName = "Артём",
+            birthDate = LocalDate.of(2009, Calendar.MARCH, 15),
+            weight = BigDecimal("51.8"),
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            team = "Кинг"
+        )
+    )
+
+    val brackets: List<BracketDto> = listOf(
+        BracketGenerationService().generate(
+            tournamentName = "Первенство Московской области по Панкратиону",
+            category = Category(
+                birthYearRange = Year.of(2009)..Year.of(2010),
+                weightCategory = WeightCategory(52)
+            ),
+            participants = participants
+        )
+    )
+
+    TemplateService(FileService()).process(brackets)
 }

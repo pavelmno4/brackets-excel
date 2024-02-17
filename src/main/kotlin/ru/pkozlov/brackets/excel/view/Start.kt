7e44e311fa2ac2ebe3bacad753e174706d984438
Pavel Kozlov <pavel.mno4@gmail.com>
2024-02-17 19:56:27 +0300

    join core and view

--- src/main/kotlin/ru/pkozlov/brackets/excel/view/Start.kt
+++ src/main/kotlin/ru/pkozlov/brackets/excel/view/Start.kt
@@ -12,12 +12,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import org.kodein.di.instance
+import ru.pkozlov.brackets.excel.core.di.di
+import ru.pkozlov.brackets.excel.core.service.BracketService
 import ru.pkozlov.brackets.excel.view.shapes.Triangle
 
 @Composable
 fun Start(files: MutableState<List<String>>) {
+    val bracketService: BracketService by di.instance()
+
     OutlinedButton(
-        onClick = {},
+        onClick = { bracketService.createBrackets(files.value) },
         modifier = Modifier
             .padding(top = 10.dp)
             .size(width = 160.dp, height = 160.dp),
